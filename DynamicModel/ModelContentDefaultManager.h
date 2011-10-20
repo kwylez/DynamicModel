3721eba4104b0e9879a1b86c2d8dfebf1ad65767
Cory D. Wiles <kwylez@gmail.com>
2011-10-19 22:00:59 -0400

    ability to switch out the json files for different models

--- DynamicModel/ModelContentDefaultManager.h
+++ DynamicModel/ModelContentDefaultManager.h
@@ -8,7 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-#define kModelJsonFile @"models"
+//#define kModelJsonFile @"employees"
+#define kModelJsonFile @"events"
 
 @interface ModelContentDefaultManager : NSObject {}
 
